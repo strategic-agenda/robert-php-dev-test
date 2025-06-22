c1da059db820387a5616fa3ca3ece0ee23fcaa21
personal <primastech101@gmail.com>
2025-06-22 19:38:41 +0100

    fix error logging

--- api/index.php
+++ api/index.php
@@ -2,9 +2,13 @@
 
 //log errors
 error_reporting(E_ALL);
-ini_set('display_errors', 1);
 ini_set('log_errors', 1);
-ini_set('error_log', 'php_errors.log');
+ini_set('log_errors', 1);
+ini_set('error_log', __DIR__ . '/api_errors.log');
+
+
+
+// Autoload dependencies
 
 
 require_once __DIR__ . '/../vendor/autoload.php';
